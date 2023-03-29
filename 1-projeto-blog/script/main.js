6755b6d0ee18a46e740e926e547118a58af797df
Leonardo Ferreira <leonardoferreira172001@gmail.com>
2023-03-29 03:19:34 -0300

    atualização

--- 1-projeto-blog/script/main.js
+++ 1-projeto-blog/script/main.js
@@ -1,10 +1,8 @@
 let num;
 
-//gera um número de curtida aleatório
-let aleatorio = Math.random(); //gera um valor aleatorio entr 0 e 1
+let aleatorio = Math.random();
 num = 1 + Math.trunc(99 * aleatorio);
 
-//atribui um valor aleatório de like para cada card
 let curtida1 = document.getElementById("card1").innerHTML = num 
 
 let curtida2 = document.getElementById("card2").innerHTML = Math.trunc(num * Math.random())
@@ -13,48 +11,58 @@ let curtida3 = document.getElementById("card3").innerHTML = Math.trunc(num * Mat
 
 let curtida4 = document.getElementById("card4").innerHTML = Math.trunc(num * Math.random())
 
-
-//funções
 function curtir1(){
-    let cont=curtida1 + 1;
+    let elemento = document.getElementById("core1");
 
-    let elemento = document.getElementById("core1")
-
-    elemento.classList.remove("bi-heart")
-    elemento.classList.add("bi-heart-fill")
-
-    document.getElementById("card1").innerHTML = cont; 
+    if (elemento.classList.contains("bi-heart")) {
+        elemento.classList.remove("bi-heart");
+        elemento.classList.add("bi-heart-fill");
+        document.getElementById("card1").innerHTML = curtida1 + 1;
+    } else {
+        elemento.classList.remove("bi-heart-fill");
+        elemento.classList.add("bi-heart");
+        document.getElementById("card1").innerHTML = curtida1;
+    }
 }
 
 function curtir2(){
-    let cont=curtida2 + 1;
+    let elemento = document.getElementById("core2");
 
-    let elemento = document.getElementById("core2")
-
-    elemento.classList.remove("bi-heart")
-    elemento.classList.add("bi-heart-fill")
-
-    document.getElementById("card2").innerHTML = cont; 
+    if (elemento.classList.contains("bi-heart")) {
+        elemento.classList.remove("bi-heart");
+        elemento.classList.add("bi-heart-fill");
+        document.getElementById("card2").innerHTML = curtida2 + 1;
+    } else {
+        elemento.classList.remove("bi-heart-fill");
+        elemento.classList.add("bi-heart");
+        document.getElementById("card2").innerHTML = curtida2;
+    }
 }
 
 function curtir3(){
-    let cont=curtida3 + 1;
+    let elemento = document.getElementById("core3");
 
-    let elemento = document.getElementById("core3")
-
-    elemento.classList.remove("bi-heart")
-    elemento.classList.add("bi-heart-fill")
-
-    document.getElementById("card3").innerHTML = cont; 
+    if (elemento.classList.contains("bi-heart")) {
+        elemento.classList.remove("bi-heart");
+        elemento.classList.add("bi-heart-fill");
+        document.getElementById("card3").innerHTML = curtida3 + 1;
+    } else {
+        elemento.classList.remove("bi-heart-fill");
+        elemento.classList.add("bi-heart");
+        document.getElementById("card3").innerHTML = curtida3;
+    }
 }
 
 function curtir4(){
-    let cont=curtida4 + 1;
+    let elemento = document.getElementById("core4");
 
-    let elemento = document.getElementById("core4")
-
-    elemento.classList.remove("bi-heart")
-    elemento.classList.add("bi-heart-fill")
-
-    document.getElementById("card4").innerHTML = cont; 
+    if (elemento.classList.contains("bi-heart")) {
+        elemento.classList.remove("bi-heart");
+        elemento.classList.add("bi-heart-fill");
+        document.getElementById("card4").innerHTML = curtida4 + 1;
+    } else {
+        elemento.classList.remove("bi-heart-fill");
+        elemento.classList.add("bi-heart");
+        document.getElementById("card4").innerHTML = curtida4;
+    }
 }
